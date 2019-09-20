ede580091b44339e47b1b593d75f2487f04c080b
Eric Patrick <epatrick@quandis.com>
2019-09-20 08:59:49 -0400

    Added proof of concept HttpListener to enable app servers to listen for SQS events.

---
+++ src/qbo4.Listener/Listener.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace qbo4.Listener
+{
+    public static class Listener
+    {
+        private static HttpListener server;
+        private static bool IsRunning;
+
+        public async static void Start()
+        {
+            var prefixes = new string[] { "http://localhost:8081/test/" };
+            server = new HttpListener();
+            foreach (string s in prefixes)
+                server.Prefixes.Add(s);
+
+            server.Start();
+            IsRunning = true;
+            Console.WriteLine("Listening...");
+            // Note: The GetContext method blocks while waiting for a request. 
+            // Obtain a response object.
+            while (server.IsListening && (server != null))
+            {
+                try
+                {
+                    var context = await server.GetContextAsync();
+                    Process(context);
+                }
+                catch (System.ObjectDisposedException)
+                {
+                    // this is okay
+                }
+            }
+        }
+
+        private static async Task Process(HttpListenerContext context)
+        {
+            var response = context.Response;
+
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes("<HTML><BODY> Hello world!</BODY></HTML>");
+            response.ContentLength64 = buffer.Length;
+            using (var stream = response.OutputStream)
+            {
+                stream.Write(buffer, 0, buffer.Length);
+            }
+        }
+
+        public static void Stop()
+        {
+            IsRunning = false;
+            // server.Close();
+            server.Stop();
+        }
+
+        private static void ListenerCallback(IAsyncResult ar)
+        {
+            Console.WriteLine("request processed");
+        }
+    }
+}
